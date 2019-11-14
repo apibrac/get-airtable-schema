1ea36595e4222f709d112a9894c8c264db20c84a
apibrac <alexis.pibrac@gmail.com>
2019-11-14 18:41:20 +0100

    Generalize to any base

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const axios = require("axios");
 const reg = /<script>\nwindow.initData = (.*);\n<\/script>/g;
 
 exports.fetch = async function(req, res) {
-  const { data } = await axios.get("https://airtable.com/shrtOxOZisVlHWq9x");
+  const { id } = req.query;
+  const { data } = await axios.get(`https://airtable.com/${id}`);
   const initData = reg.exec(data)[1];
-  const { accessPolicy } = JSON.parse(initData);
+  const { accessPolicy, sharedApplicationId: appId } = JSON.parse(initData);
 
   const response = await axios.get(
-    "https://airtable.com/v0.3/application/apprwW5vVR4F8HP9N/read",
+    `https://airtable.com/v0.3/application/${appId}/read`,
     {
       params: {
         accessPolicy
@@ -17,7 +18,7 @@ exports.fetch = async function(req, res) {
         Connection: "keep-alive",
         Pragma: "no-cache",
         "Cache-Control": "no-cache",
-        "x-airtable-application-id": "apprwW5vVR4F8HP9N",
+        "x-airtable-application-id": appId,
         "ot-tracer-sampled": "true",
         Accept: "application/json, text/javascript, */*; q=0.01",
         "x-time-zone": "Europe/Paris",
@@ -27,8 +28,7 @@ exports.fetch = async function(req, res) {
         "Sec-Fetch-Mode": "cors",
         "Accept-Encoding": "gzip, deflate, br",
         "Accept-Language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-        "cache-control": "no-cache",
-        "Postman-Token": "1738e350-ed77-48de-93fa-ad751dad0383"
+        "cache-control": "no-cache"
       }
     }
   );
